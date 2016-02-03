0f7d3fc4428b36539d88ff8acc4b1914c21452cf
Doug Wollison <doug@premise.ca>
2016-02-03 15:36:59 -0500

    Added JSON export on save

--- js/interface.js
+++ js/interface.js
@@ -1,2 +1,20 @@
+/* globals POMOEdit */
 jQuery( function( $ ) {
+	$( '#pomoedit-manage' ).submit( function() {
+		var Project = POMOEdit.Project;
+		var $storage = $('<textarea name="pomoedit_data"></textarea>').hide().appendTo(this);
+
+		var data = {
+			entries: []
+		};
+		for ( var attr in Project.attributes ) {
+			data[attr] = Project.get( attr );
+		}
+
+		Project.Translations.each( function( entry ) {
+			data.entries.push( entry.attributes );
+		} );
+
+		$storage.val( JSON.stringify( data ) );
+	} );
 } );
