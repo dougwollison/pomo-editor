10c0df984085a96b8790d82d71b66c5f83c61020
Doug Wollison <Doug@Dougs-iMac.local>
2016-03-30 15:53:34 -0400

    Don't need these classes

--- includes/class-pomoedit-system.php
+++ includes/class-pomoedit-system.php
@@ -30,41 +30,14 @@ final class System extends Handler {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @uses Registry::load() to load the options.
-	 * @uses Loader::register_hooks() to setup plugin management.
-	 * @uses System::register_hooks() to setup global functionality.
 	 * @uses Backend::register_hooks() to setup backend functionality.
-	 * @uses AJAX::register_hooks() to setup AJAX functionality.
 	 * @uses Manager::register_hooks() to setup admin screens.
 	 * @uses Documenter::register_hooks() to setup admin documentation.
 	 */
 	public static function setup() {
-		// Setup the registry
-		Registry::load();
-
-		// Register the Installer stuff
-		Installer::register_hooks();
-
-		// Register global hooks
-		static::register_hooks();
-
 		// Register the hooks of the subsystems
 		Backend::register_hooks();
-		AJAX::register_hooks();
 		Manager::register_hooks();
 		Documenter::register_hooks();
-	}
-
-	// =========================
-	// ! Setup Utilities
-	// =========================
-
-	/**
-	 * Register hooks.
-	 *
-	 * @since 1.0.0
-	 */
-	public static function register_hooks() {
-		// to be written
 	}
 }
