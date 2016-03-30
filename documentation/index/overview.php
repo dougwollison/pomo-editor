279ce74126de848683242be8bfba3a7ddb97b09a
Doug Wollison <Doug@Dougs-iMac.local>
2016-03-30 11:46:03 -0400

    Started help documentation

---
+++ documentation/index/overview.php
@@ -0,0 +1,3 @@
+<title><?php _e( 'Overview', 'wp-cloak' ); ?></title>
+
+<p><?php _e( 'Below is a listing of all translation files found on your site (within the allowed directories).', 'wp-cloak' ); ?></p>
