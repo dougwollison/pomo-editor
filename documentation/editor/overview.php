5bb8a8ffd2767348126c429002769066beade31d
Doug Wollison <Doug@Dougs-iMac.local>
2016-03-30 15:54:10 -0400

    Fleshed out help tab content

--- documentation/editor/overview.php
+++ documentation/editor/overview.php
@@ -1,3 +1,9 @@
 <title><?php _e( 'Overview', 'pomoedit' ); ?></title>
 
-<p><?php _e( 'Below you can edit the selected translation file, updating translation entries as needed.', 'pomoedit' ); ?></p>
+<p><?php _e( 'This screen allows you to edit the individual translation entries in the selected file. ', 'pomoedit' ); ?></p>
+
+<p><?php printf( __( 'Click the %s icon to open an entry for editing. By default, the source inputs are disabled from editing; click to enable them should you actually want to change these. When you’re done with your changes to an entry, click the %2$s icon to save, or otherwise the %3$s icon to discard your changes. ', 'pomoedit' ), '<i class="dashicons dashicons-edit"></i>', '<i class="dashicons dashicons-yes"></i>', '<i class="dashicons dashicons-no"></i>' ); ?></p>
+
+<p><?php _e( 'When you’re done editing translation entries, click the <strong>Save Translations</strong> button to update the .po file and regenerate the .mo file.', 'pomoedit' ); ?> <em><?php _e( 'It is recommended you backup the original translation files to be safe, in the event an error occurs with updating/compiling.', 'pomoedit' ); ?></em></p>
+
+<p><?php _e( 'Some entries include a <em>context</em> qualifier, which allows multiple translations of the same basic text for different uses. The context for an entry cannot be changed.', 'pomoedit' ); ?></p>
--- documentation/index/overview.php
+++ documentation/index/overview.php
@@ -1,3 +1,3 @@
 <title><?php _e( 'Overview', 'pomoedit' ); ?></title>
 
-<p><?php _e( 'Below is a listing of all translation files found on your site (within the allowed directories).', 'pomoedit' ); ?></p>
+<p><?php _e( 'Below is a listing of all translation files found on your site (within the allowed directories). You can filter the selection by type, package, and/or language to find the one you want to edit. To open a file for editing, click the file’s path/name.', 'pomoedit' ); ?></p>
