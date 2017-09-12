cc049b05f68c77a6a33ac409fc2f65f47dabe69d
Doug Wollison <doug@wollison.net>
2017-09-12 15:32:32 -0400

    Dropped defunct medataeditor code

--- js/interface.js
+++ js/interface.js
@@ -33,7 +33,6 @@ jQuery( function( $ ) {
 		// Turn off read-only on all fields and strip from templates
 		$( '.pme-input' ).attr( 'readonly', false );
 		POMOEditor.HeadersEditor.rowTemplate      = POMOEditor.HeadersEditor.rowTemplate.replace( /readonly/g, '' );
-		POMOEditor.MetadataEditor.rowTemplate     = POMOEditor.MetadataEditor.rowTemplate.replace( /readonly/g, '' );
 		POMOEditor.TranslationsEditor.rowTemplate = POMOEditor.TranslationsEditor.rowTemplate.replace( /readonly/g, '' );
 	} );
 
